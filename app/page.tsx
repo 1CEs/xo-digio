3ed904b9cb22e55f5f8b8271a7cee55400ce4c43
1CEs <isongwut.me@gmail.com>
2025-08-01 22:41:33 +0700

    chore: calling comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
-import Image from "next/image";
+import PlayfulButton from "@/components/playful-button";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <main className="flex flex-col gap-4 items-center justify-center h-full">
+      <PlayfulButton size="xl" variant="primary">
+        Sign Up
+      </PlayfulButton>
+      <PlayfulButton size="lg" variant="secondary">
+        Sign In
+      </PlayfulButton>
+      <PlayfulButton size="md" variant="default">
+        Continue as guest
+      </PlayfulButton>
+    </main>
   );
 }
