cb4934cd4140cdc1418d9dfc44695c1e85addb2d
1CEs <isongwut.me@gmail.com>
2025-08-02 10:42:35 +0700

    add: sign-up & sign-in form validation

---
+++ validation/form.ts
@@ -0,0 +1,30 @@
+import z from "zod"
+export const signUpSchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters long"),
+    email: z.string().email("Invalid email"),
+    password: z.string()
+                    .min(8, "Password must be at least 8 characters long")
+                    .uppercase()
+                    .lowercase()
+                    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 
+                    "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+    confirmPassword: z.string()
+                        .min(8, "Password must be at least 8 characters long")
+                        .uppercase()
+                        .lowercase()
+                        .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, 
+                        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"),
+}).superRefine(({confirmPassword, password}, ctx) => {
+    if (confirmPassword !== password) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Passwords do not match",
+            path: ["confirmPassword"]
+        });
+    }
+})
+
+export const signInSchema = z.object({
+    email: z.string().email("Invalid email"),
+    password: z.string(),
+})
